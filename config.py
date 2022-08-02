7db8d158afb43a3d40d08386e36c19108e5f08d7
Nioz <48495934+Nioz@users.noreply.github.com>
2022-08-02 17:33:58 -0400

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
 # This will be changed to the target email
-fromAddr = '.' #Put email you wish file to be sent to here
-fromPswd = '.' #Enter said email's password here
+fromAddr = 'loremipsum@email.com' #Put email you wish file to be sent to here
+fromPswd = 'loremspassword' #Enter said email's password here
